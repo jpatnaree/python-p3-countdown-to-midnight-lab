b9c807d1a48159dde6420c71fc6d45e5651239c3
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-11-13 14:50:02 -0500

    lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+def countdown(number):
+    i = number
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        i -= 1
+    print("HAPPY NEW YEAR!")
+
+from time import sleep
+
+def countdown_with_sleep(number):
+    i = number
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        i -= 1
+        sleep(1)
+    print("HAPPY NEW YEAR!")
